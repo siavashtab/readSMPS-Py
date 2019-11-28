038220a939761a315ef30bdc03f8b97b13616939
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2019-11-28 16:06:25 -0600

    sub

--- readSMPS/decmps_2slp.py
+++ readSMPS/decmps_2slp.py
@@ -107,6 +107,19 @@ class decompose:
             LinObj += (vdic - var_dic[vdic]) * (vdic - var_dic[vdic])
         return LinObj
     
+    # replace the observations in the proper location of the problem
+    def replaceObs(self,obs,constr):
+        obscount = 0
+        for c in constr:
+            if c.getAttr('ConstrName') in self.RV.rv:
+                if len(obs) < obscount-1:
+                    print("ERROR: Length of obs is less than rvs")
+                    break;
+                else:
+                    c.setAttr('RHS',obs[obscount])
+                    obscount += 1
+        return constr
+    
     #Create master constraints
     def create_master_constr(self):
         constr = self.prob.mean_const[:self.tim.stage_idx_row[1]]
@@ -119,14 +132,18 @@ class decompose:
         self.prob.master_const = self.prob.master_model.getConstrs()
     
     #Create LSsub constraints
-    def create_LSsub_constr(self,incmbt):
+    def create_LSsub_constr(self,obs,incmbt):
         constr = self.prob.mean_const[:self.tim.stage_idx_row[1]]
+        constr = self.replaceObs(obs,constr)
         for c in constr:
             empt = gb.LinExpr()
-            for v in self.prob.master_vars:
+            Cx = 0
+            for v in self.prob.sub_vars:
                 empt += self.mean_model.getCoeff(c,v) * v
-            self.prob.master_model.addConstr(empt,c.getAttr('Sense'),c.getAttr('RHS'),c.getAttr('ConstrName'))
-        self.prob.master_const = self.prob.master_model.getConstrs()
+            for v in range(len(self.prob.master_vars)):
+                Cx   += self.mean_model.getCoeff(c,self.prob.master_vars[v]) * incmbt[v]
+            self.prob.sub_model.addConstr(empt,c.getAttr('Sense'),c.getAttr('RHS') - Cx,c.getAttr('ConstrName'))
+        self.prob.sub_const = self.prob.sub_model.getConstrs()
     
     #Creating linear master with one surrogates (\eta)
     def create_master(self):
@@ -255,12 +272,12 @@ class decompose:
     def create_LSsub(self,obs,incmb):
         self.prob.sub_vars  = self.prob.sub_vars[self.tim.stage_idx_col[1]:]
         self.prob.sub_const = self.prob.sub_const[self.tim.stage_idx_col[1]:]
-        fvarName = [j.getAttr("VarName") for j in self.prob.master_vars ]
-        
+
         for v in self.prob.sub_vars:
-            self.prob.master_model.addVar(lb=v.getAttr("LB"), ub=v.getAttr("UB"), obj=v.getAttr("Obj"), vtype=v.getAttr("VType"), name=v.getAttr("VarName"))
-        self.prob.master_model.update()
-        self.prob.master_vars = self.prob.master_model.getVars()
+            self.prob.sub_model.addVar(lb=v.getAttr("LB"), ub=v.getAttr("UB"), obj=v.getAttr("Obj"), vtype=v.getAttr("VType"), name=v.getAttr("VarName"))
+        self.prob.sub_model.update()
+        self.prob.sub_vars = self.prob.sub_model.getVars()
+        self.create_LSsub_constr(obs,incmbt)
     
         
         
