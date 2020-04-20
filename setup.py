41c4c51c59be07a04d13ccf66558961fe5761945
Siavash Tabrizian <30736084+siavashtab@users.noreply.github.com>
2020-04-19 20:13:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(name='ReadSMPS',
       version='0.0.1',
-      description='reading SMPs format files',
-      long_description='Using this package the read the SMPS files using GUROBI and python',
+      description='reading SMPS format files',
+      long_description='Using this package to read the SMPS files in python',
       keywords='readSMPS',
       url='https://github.com/siavashtab/readSMPS-Py',
       author='Siavash Tabrizian',
       author_email='stabrizian@smu.edu',
       license='Siavash Tabrizian',
-      packages=find_packages(),
+      packages=find_packages(gurobipy),
       include_package_data=True,
       zip_safe=False)
